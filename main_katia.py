e35ad35ce2dabe2de3044614e3de8174ecdbd0d6
kabelev18 <skr3sr@virginia.edu>
2026-09-16 15:57:59 -0400

    katia coding assignment
    created a patient file and coded in my main file

--- main_katia.py
+++ main_katia.py
@@ -4,3 +4,77 @@ df = pd.read_csv("C:\\Users\\kabel\\OneDrive\\Desktop\\Comp BME\\Mod 1\\Fung5_Mo
 
 for header in df.columns:
     print(header)
+
+from patient_katia import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+
+Patient.instantiate_from_csv("C:\\Users\\kabel\\OneDrive\\Desktop\\Comp BME\\Mod 1\\Fung5_Module1\\Metadata and Protein Data for Module 1.csv")
+
+for patient in Patient.all_patients:
+    print(patient)
+
+# sorting patients by age at death
+Patient.all_patients.sort(key=Patient.get_age_at_death, reverse=False)
+
+print("Patients sorted by age at death: ")
+for patient in Patient.all_patients:
+    print(patient)
+
+# filter subset of patients based on 2 attributes: female patients with years of education > 12
+female_educated_patients = Patient.filter(Patient.all_patients, sex="Female", years_of_ed=12)
+print("Female patients with years of education > 12: ")
+
+for patient in female_educated_patients:
+    print(patient)
+
+# making a bar graph of different Amyloid-Beta40 levels in Males vs. Females
+female_amyloid = []
+male_amyloid = []
+
+for patient in Patient.all_patients:
+    if patient.sex == "Female":
+        female_amyloid.append(patient.ABeta40)
+    if patient.sex == "Male":
+        male_amyloid.append(patient.ABeta40)
+
+female_mean = statistics.mean(female_amyloid)
+male_mean = statistics.mean(male_amyloid)
+
+female_stdev = statistics.stdev(female_amyloid)
+male_stdev = statistics.stdev(male_amyloid)
+
+print("Female Amyloid-Beta40 Mean: ")
+print(f"Mean = {female_mean}")
+print(f"Standard deviation = {female_stdev}")
+
+print("Male Amyloid-Beta40 Mean: ")
+print(f"Mean = {male_mean}")
+print(f"Standard deviation = {male_stdev}")
+
+sex_labels = ["Female", "Male"]
+mean_sex = [female_mean, male_mean]
+stdev_sex = [female_stdev, male_stdev]
+yerr = [np.zeros(len(mean_sex)), stdev_sex]
+
+plt.bar(sex_labels, mean_sex, yerr=yerr, capsize=10)
+plt.title("Average Amyloid-Beta40 Levels by Sex")
+plt.xlabel("Sex")
+plt.ylabel("Average Amyloid-Beta40 Levels")
+plt.show()
+
+# making a scatter plot of years of education vs. age at death
+education = []
+age_at_death = []
+
+for patient in Patient.all_patients:
+    education.append(patient.years_of_ed)
+    age_at_death.append(patient.age_at_death)
+
+plt.scatter(education, age_at_death, color='blue')
+plt.title("Years of Education vs. Age at Death")
+plt.xlabel("Years of Education")
+plt.ylabel("Age at Death")
+plt.show()
---
+++ patient_katia.py
@@ -0,0 +1,53 @@
+import csv
+
+class Patient:
+
+    def __init__(self, patient_id: str, age_at_death: int, sex: str, years_of_ed: int, ABeta40: float, tTAU: float):
+        self.patient_id = patient_id
+        self.age_at_death = age_at_death
+        self.sex = sex
+        self.years_of_ed = years_of_ed
+        self.ABeta40 = ABeta40
+        self.tTAU = tTAU
+
+        Patient.all_patients.append(self)
+
+    def __repr__(self):
+        return f"{self.patient_id}: ({self.age_at_death} | {self.sex} | {self.years_of_ed} | {self.ABeta40} | {self.tTAU})"
+
+    def get_age_at_death(self):
+        return self.age_at_death
+
+    all_patients = []
+    @classmethod
+    def instantiate_from_csv(cls, filename: str):
+
+        with open(filename, encoding="utf8") as f:
+            reader = csv.DictReader(f)
+            rows_of_patients = list(reader)
+
+            for row in rows_of_patients:
+                Patient(
+                    patient_id=row['Donor ID'],
+                    age_at_death=int(row['Age at Death']),
+                    sex=row['Sex'],
+                    years_of_ed=int(row['Years of education']),
+                    ABeta40=float(row['ABeta40 pg/ug']),
+                    tTAU=float(row['tTAU pg/ug'])
+                )
+
+    @classmethod
+    def filter(cls, list, sex="any", years_of_ed="any"):
+
+        all_patients = list
+        remove_list = []
+
+        for patient in all_patients:
+            if sex != "any" and patient.sex != sex:
+                remove_list.append(patient)
+            elif years_of_ed != "any" and patient.years_of_ed <= years_of_ed:
+                remove_list.append(patient)
+
+        all_patients = [patient for patient in all_patients if patient not in remove_list]
+
+        return all_patients
